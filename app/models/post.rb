994bf94651eb99127ac5a2b4039a378bba91522b
ashkaren <gvevtodiev@gmail.com>
2016-03-26 17:41:30 -0400

    minori

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   mount_uploader :attachment, AvatarUploader
+  validates :attachment, allow_blank: true, format: {
+	with:
+	%r{\.(gif|jpg|png)\Z}i,
+	message: 'must be a URL for GIF, JPG or PNG image.'
+	}
 end
