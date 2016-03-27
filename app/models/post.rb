a69f00b084bb911c58944ec9306d2a989b618c76
ashkaren <gvevtodiev@gmail.com>
2016-03-26 22:29:32 -0400

    likes comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+  acts_as_votable
+  acts_as_commentable
 
   mount_uploader :attachment, AvatarUploader
   validates :attachment, allow_blank: true, format: {
