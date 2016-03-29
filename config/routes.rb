40bdfe8b376f5079c4d4906522c6ab51fe061226
Jason Castillo <jhcastillo@valdosta.edu>
2016-03-29 17:35:15 -0400

    Revamped website...major fixes and updates with bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
 
-  resources :posts
-  devise_for :users
-  resources :users
-  get "/users/:id(.:format)", to: "users#show", as: "feed"
+  	resources :posts
+  	devise_for :users
+  	resources :users
+  	get "/users/:id(.:format)", to: "users#show", as: "feed"
 
-  get "welcome/index"
-  root "welcome#index", as: 'welcome'
+	authenticated :user do
+    	root to: 'home#index', as: 'home'
+  	end
+ 	unauthenticated :user do
+    	root 'welcome#index', as: 'welcome'
+  	end
 
-  get "/about", to: "static_pages#about", as: 'about'
-  get "/contact", to: "static_pages#contact", as: 'contact'
-  get "/help", to: "static_pages#help", as: 'help'
+  	get "/about", to: "static_pages#about", as: 'about'
+  	get "/contact", to: "static_pages#contact", as: 'contact'
+  	get "/help", to: "static_pages#help", as: 'help'
+  	
 end
